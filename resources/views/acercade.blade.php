383c015d5bb2c98eca3d5446e3c3e6ed94e24fab
yeraldinzulena00 <105450747+yeraldinzulena00@users.noreply.github.com>
2022-11-30 18:05:49 -0500

    first commit

---
+++ resources/views/acercade.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    @vite('resources/css/acercade.css')
+    <title>Document</title>
+</head>
+<body>
+
+<header>
+    @include('header')
+</header>
+<main>
+    <h1>Acerca de</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consequatur debitis doloribus esse fugit porro soluta? A dolore doloribus quae recusandae voluptate? Consequatur fugiat, laudantium quaerat rem saepe similique vitae.</p>
+    <p></p>
+    <button class="btn btn-primary btn-lg">Enviar</button>
+</main>
+<footer>
+    @include('footer')
+</footer>
+
+</body>
+</html>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,13 @@
+<nav>
+    <ul>
+        <li>
+            <a href="{{asset('inicio')}}">inicio</a>
+        </li>
+        <li>
+            <a href="{{asset('pag_acercade')}}">acercade</a>
+        </li>
+        <li>
+            <a>ayuda</a>
+        </li>
+    </ul>
+</nav>
